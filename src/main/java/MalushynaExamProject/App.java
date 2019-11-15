a3a5ea4025488cdc39347a1337abdb16d8ab4c56
EvheniiaM <jonsonseee@gmail.com>
2019-11-15 11:20:22 +0200

    first commit

---
+++ src/main/java/MalushynaExamProject/App.java
@@ -0,0 +1,13 @@
+package MalushynaExamProject;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
